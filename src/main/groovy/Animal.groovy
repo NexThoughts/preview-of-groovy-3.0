231bc2377cb4596e69816f9c42f0c6d7865f6de4
Vijay <vshukla684@gmail.com>
2018-09-17 22:23:32 +0530

    References Example

---
+++ src/main/groovy/Animal.groovy
@@ -0,0 +1,8 @@
+package main.groovy
+
+import groovy.transform.Canonical
+
+@Canonical
+class Animal {
+    String kind
+}
---
+++ src/main/groovy/ReferencesExample.groovy
@@ -0,0 +1,43 @@
+package main.groovy
+
+import java.util.stream.Stream
+import static java.util.stream.Collectors.toList
+
+class ReferencesExample {
+    public static void main(String[] args) {
+        example1()
+        example2()
+        example3()
+        example4()
+        example5()
+    }
+
+    static void example1() {
+        List data = Stream.of(1, 2, 3)
+                .map(String:: valueOf)
+                .collect(toList())
+
+        println data
+    }
+
+    static void example2() {
+        List data = ['a', 'b', 'c'].stream()
+                .map(String:: toUpperCase)
+                .collect(toList())
+        println data
+    }
+
+    static void example3() {
+        def r = Random:: new
+                println r().nextInt(10) in 0..9
+    }
+
+    static void example4() {
+        def a = Animal::new
+        println a('lion').kind
+    }
+    static void example5() {
+        def c = Animal
+        println c::new('cat').kind
+    }
+}
