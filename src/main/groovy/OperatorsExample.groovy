deff39a177145c84bce23cb3d95a1351ca52d00e
Vijay <vshukla684@gmail.com>
2018-09-18 22:55:24 +0530

    Operators Example

---
+++ src/main/groovy/OperatorsExample.groovy
@@ -0,0 +1,54 @@
+package main.groovy
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+
+class OperatorsExample {
+    public static void main(String[] args) {
+        notInExample()
+        notInstanceOfExample()
+        elvisExample()
+        identityComparison()
+    }
+
+    static void notInExample() {
+        println(4 !in [1, 3, 5, 7])
+    }
+
+    static void notInstanceOfExample() {
+        println(45 !instanceof Date)
+    }
+
+    static void elvisExample(){
+        def he = new Element(name: 'Helium')
+        he.with {
+            name = name ?: 'Hydrogen'
+            atomicNumber ?= 2
+        }
+
+        println(he.toString() == 'Element(Helium, 2)')
+    }
+
+    static void identityComparison(){
+        def cat = new Creature(type: 'cat')
+        def copyCat = cat
+        def lion = new Creature(type: 'cat')
+
+        println(cat.equals(lion))
+        println(cat == lion)
+
+        //Groovy
+        println(cat.is(copyCat))
+        println(cat === copyCat)
+        println(cat !== lion)
+    }
+}
+
+@ToString
+class Element {
+    String name
+    int atomicNumber
+}
+
+@EqualsAndHashCode
+class Creature { String type }
